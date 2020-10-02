49e3295e4e151c02fb065c86dff963169bc28733
Rahul Kumar <rahul.kr0324@gmail.com>
2020-10-02 13:20:04 +0530

    Created SumClosest2Zero.cpp

---
+++ SumClosest2Zero.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+bool compare(int x, int y) 
+{ 
+    return abs(x) < abs(y); 
+} 
+  
+void findMinSum(int arr[], int n) 
+{ 
+    sort(arr, arr + n, compare); 
+    int min = INT_MAX, x, y; 
+    for (int i = 1; i < n; i++) { 
+
+        if (abs(arr[i - 1] + arr[i]) <= min) { 
+ 
+            min = abs(arr[i - 1] + arr[i]); 
+            x = i - 1; 
+            y = i; 
+        } 
+    } 
+    cout << "The two elements whose sum is minimum are "
+         << arr[x] << " and " << arr[y]; 
+} 
+ 
+int main() 
+{ 
+  
+    int arr[] = { 1, 60, -10, 70, -80, 85 }; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    findMinSum(arr, n); 
+    return 0; 
+} 
